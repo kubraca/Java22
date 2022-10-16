8d2e063402a6c9ae3e7604ef603067fbaa266099
Kubra <kubracelebi279@gmail.com>
2022-10-16 06:48:28 +0300

    kodlama.io homework is finished..

---
+++ src/OopWithNLayeredHomework/entites/ProductEntity.java
@@ -0,0 +1,31 @@
+package OopWithNLayeredHomework.entites;
+
+public class ProductEntity {
+    private int id;
+    private String name;
+    private double unitPrice;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+}
